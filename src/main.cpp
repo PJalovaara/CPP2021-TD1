530832f5d4c3d784148b6ca162f25939ae1a2b83
Atte Tommiska <atte.tommiska@aalto.fi>
2021-11-18 09:27:08 +0200

    Add an example of how to use Doxygen to main.cpp
    The example is a bit crude but should give our coders a decent idea of
    what they should write as comments.

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,17 @@
 #include <QApplication>
 #include "game.hpp"
-
-
+// This is an example of how to use doxygen
+/**
+ * @brief Here we put a brief description, that will end on the first dot, newline etc.
+ * 
+ * Meaning that this here is the detailed description. This can be much longer than the brief
+ * description.
+ * 
+ * @param argc using (at)param means that doxygen will create documentation for this parameter
+ * @param argv meaning that we can say something about the parameter in hand
+ * @return int and here we can say smth about the return value
+ * @see Game() this will give the user a link in the documentation to the linked function/class.
+ */
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
